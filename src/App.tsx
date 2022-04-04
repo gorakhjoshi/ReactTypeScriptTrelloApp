70005772121e4399dc2d0ab8c01d2a32768ad9b3
Gorakh Raj Joshi <gorakh626@gmail.com>
2022-04-04 07:34:49 +0545

    components with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import { AppContainer } from "./styles/styles";
+import Column from "./components/Column";
+import Card from "./components/Card";
 
 function App() {
-  return <AppContainer>App</AppContainer>;
+  return (
+    <AppContainer>
+      <Column text="To Do">
+        <Card text="Make TypeScript Project" />
+      </Column>
+      <Column text="In Progress">
+        <Card text="Make TypeScript Project" />
+      </Column>
+      <Column text="Completed!">
+        <Card text="Make TypeScript Project" />
+      </Column>
+    </AppContainer>
+  );
 }
 
 export default App;
